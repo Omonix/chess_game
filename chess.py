7d16293a400d0ad3d5cd6d0c5594e7cb2281b2b8
Louis Blonde <loul0uboypy@gmail.com>
2025-01-22 21:22:44 +0100

    good verifing

--- chess.py
+++ chess.py
@@ -62,7 +62,7 @@ def lb_play(g, fromer, toer):
 def lb_can_play(g, here, to, player):
     fromer = lb_convert_xy(here)
     toer = lb_convert_xy(to)
-    if int(g[fromer[1]][fromer[0]][0]) != player or g[toer[1]][toer[0]] == '■' or g[toer[1]][toer[0]] == '□':
+    if int(g[fromer[1]][fromer[0]][0]) != player or g[fromer[1]][fromer[0]] == '■' or g[fromer[1]][fromer[0]] == '□':
         return False
     if ((g[fromer[1]][fromer[0]][1] == 'P' and (fromer[1] - 1 == toer[1] and player == 2)) or (fromer[1] + 1 == toer[1] and player == 1)) or (g[fromer[1]][fromer[0]][1] == 'B' and (abs(fromer[1] - toer[1]) == abs(fromer[0] - toer[0]))) or (g[fromer[1]][fromer[0]][1] == 'R' and (fromer[0] == toer[0] or toer[1] == fromer[1])):
         lb_play(g, fromer, toer)
@@ -76,7 +76,7 @@ while True:
     if player == 1:
         todo = input(lb_colored('Player 1 : ', GREEN)).split(' ')
     else:
-        todo = input(lb_colored('Player 2 : ', BLUE))
+        todo = input(lb_colored('Player 2 : ', BLUE)).split(' ')
     if todo[0] == 'play':
         if not(lb_can_play(grid, todo[1], todo[2], player)):
             print(lb_colored('Error move impossible', RED))
